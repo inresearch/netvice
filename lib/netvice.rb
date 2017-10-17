e52a4efa0be7c19b76be256db8954dc67497dbc5
Adam Pahlevi <adam.pahlevi@gmail.com>
2017-10-17 21:46:18 +0700

    init

---
+++ lib/netvice.rb
@@ -0,0 +1,5 @@
+require "netvice/version"
+
+module Netvice
+  # Your code goes here...
+end
---
+++ spec/netvice_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Netvice do
+  it "has a version number" do
+    expect(Netvice::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
