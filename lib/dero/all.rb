341cd230e019248aeea24697e46edf2abac044e2
Adam Pahlevi <adam.pahlevi@gmail.com>
2017-11-01 08:35:24 +0700

    add missing

--- lib/dero/all.rb
+++ lib/dero/all.rb
@@ -1,5 +1,7 @@
 module Dero
   autoload :Configuration, "dero/configuration"
+  autoload :StaticMethods, "dero/static_methods"
+  autoload :Processor, "dero/processor"
 
   module Kernel
     autoload :Line, "dero/kernel/line"
@@ -24,4 +26,6 @@ module Dero
     @http_conn = Netvice::Connection.new(
       conf.base_url, timeout: conf.timeout)
   end
+
+  extend Dero::StaticMethods
 end
---
+++ lib/dero/static_methods.rb
@@ -0,0 +1,9 @@
+module Dero
+  module StaticMethods
+    def capture_exception
+    end
+
+    def extra_context(contexts={})
+    end
+  end # StaticMethods
+end # Dero
